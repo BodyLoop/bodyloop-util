c70b6c80e943aec892a9b00e2fb261a9d61e02bd
basejumpa <basejumpa@encouraged-coders.de>
2026-04-16 18:02:02 +0200

    Introduce command line options --host and --port

--- bodyloop_dashboards/app.py
+++ bodyloop_dashboards/app.py
@@ -1,3 +1,5 @@
+import argparse
+
 from dash import Dash, html, dcc
 
 from bodyloop_dashboards.pages import sync_excel, scan_compare, login
@@ -22,8 +24,16 @@ app.layout = html.Div(
 )
 
 
+def _parse_args():
+    parser = argparse.ArgumentParser(description="Run BodyLoop Dashboards")
+    parser.add_argument("--host", default="127.0.0.1", help="Host interface to bind")
+    parser.add_argument("--port", type=int, default=8050, help="Port to listen on")
+    return parser.parse_args()
+
+
 def main():
-    app.run(debug=True)
+    args = _parse_args()
+    app.run(debug=True, host=args.host, port=args.port)
 
 
 if __name__ == "__main__":
